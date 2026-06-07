2d9316afa6c72aa48dfd96a6fe40cf2292c8ae0c
OmegaRaider <dima.reuder999666228@gmail.com>
2026-06-07 12:53:23 +0400

    Добавьте файлы проекта.

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Vavilon
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnLogin = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Location = new Point(290, 200);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(217, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.Location = new Point(290, 260);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(217, 23);
+            textBox2.TabIndex = 1;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.Location = new Point(350, 320);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Войти";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("a_DomIno", 18F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            label1.Location = new Point(350, 153);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 27);
+            label1.TabIndex = 3;
+            label1.Text = "VAVILON";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(180, 200);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Логин:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(180, 260);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Пароль:";
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(784, 561);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnLogin);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "LoginForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnLogin;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
